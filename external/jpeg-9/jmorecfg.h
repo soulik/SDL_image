8fa0b2f45761ac662221226b4755b5b1d4cdae23
Mário Kašuba <mario.kasuba@it-academy.sk>
2015-02-24 19:54:58 +0100

    Fixed jpeg and png library inclusion

--- external/jpeg-9/jmorecfg.h
+++ external/jpeg-9/jmorecfg.h
@@ -194,7 +194,11 @@ typedef unsigned int JDIMENSION;
 /* a function referenced thru EXTERNs: */
 #define GLOBAL(type)		type
 /* a reference to a GLOBAL function: */
-#define EXTERN(type)		extern type
+#ifdef _WIN32
+#	define EXTERN(type)		extern __declspec(dllexport) type
+#else
+#	define EXTERN(type)		extern type
+#endif
 
 
 /* This macro is used to declare a "method", that is, a function pointer.
